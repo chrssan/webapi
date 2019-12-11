6ddc347e1ecdb392590cd158e0e6b3ff0829f7bc
zuhaansari <zuhaansari98@gmail.com>
2019-12-11 11:46:31 +0000

    Added files via upload First Commit

---
+++ ABCBank/src/main/java/com/mycompany/abcbank/databases/Database.java
@@ -0,0 +1,20 @@
+package com.mycompany.abcbank.databases;
+
+import com.mycompany.abcbank.models.Customer;
+import com.mycompany.abcbank.models.Account;
+import com.mycompany.abcbank.models.Transaction;
+import java.util.ArrayList;
+import java.util.List;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author x16388923
+ */
+public class Database {
+    
+}
---
+++ ABCBank/src/main/java/com/mycompany/abcbank/models/Customer.java
@@ -0,0 +1,15 @@
+package com.mycompany.abcbank.models;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author x16388923
+ */
+public class Customer {
+    
+}
